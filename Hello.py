f773709ca1dae6bb127d26fb220d4f5033f963b7
Zining <88251341+Zining643@users.noreply.github.com>
2023-02-21 17:09:39 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,3 @@
-Hello
+user_name=input("Please enter your name")
+user_fav_artist=input("Please enter the name of your favorite artist")
+print("Hi!"+user_name+"Your favorite artist is" + user_fav_artist)
